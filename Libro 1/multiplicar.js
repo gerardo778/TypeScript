edb7a0479d752d5717e9c107e9c76dbb7183b33b
gerardo778 <54380051+gerardo778@users.noreply.github.com>
2019-08-27 19:50:45 -0600

    multiplicar

---
+++ Libro 1/multiplicar.js
@@ -0,0 +1,14 @@
+//Opcion 1
+var a = 12;
+var b = 17;
+function show_Product(first, second) {
+    console.log("El resultado es: " + first * second);
+}
+show_Product(a, b);
+//Opcion 2
+var c = 25;
+var d = 19;
+function show_Product2(first, second) {
+    console.log("El resultado es: " + first * second);
+}
+show_Product2(c, d);
---
+++ Libro 1/multiplicar.ts
@@ -0,0 +1,17 @@
+//Opcion 1
+let a: number = 12;
+let b: number = 17;
+ 
+function show_Product(first: number, second: number): void {
+    console.log("El resultado es: " +  first * second);
+}
+ 
+show_Product(a, b);
+
+//Opcion 2
+let c = 25;
+let d = 19;
+function show_Product2(first, second) {
+    console.log("El resultado es: " + first * second);
+}
+show_Product2(c, d);
