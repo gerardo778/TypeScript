284a70cff3851ee72436ededa2e2399ff82300af
gerardo778 <54380051+gerardo778@users.noreply.github.com>
2019-08-27 20:13:43 -0600

    tipo de cadenas string

---
+++ Libro 1/tipostring.js
@@ -0,0 +1,10 @@
+//ES5
+var a = "casita";
+var b = 260;
+var sentence2 = "The " + a + " in front of my office is " + b + " feet tall.";
+//ES6
+var e = "edificio";
+var f = 300;
+var sentence = "The " + e + " in front of my office is " + f + " feet tall.";
+console.log(sentence);
+console.log(sentence2);
---
+++ Libro 1/tipostring.ts
@@ -0,0 +1,14 @@
+//ES5
+var a = "casita";
+var b = 260;
+
+var sentence2 = "The " + a + " in front of my office is " + b + " feet tall.";
+
+//ES6
+let e: string = "edificio";
+let f: number = 300;
+ 
+let sentence: string = `The ${e} in front of my office is ${f} feet tall.`;
+
+console.log(sentence);
+console.log(sentence2);
