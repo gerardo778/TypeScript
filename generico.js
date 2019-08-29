86c3b472b366f890d8c938914d5760a2f457f761
gerardo778 <54380051+gerardo778@users.noreply.github.com>
2019-08-28 21:42:45 -0600

    Delete generico.js

--- generico.js
+++
@@ -1,7 +0,0 @@
-//Ejemplo de generico
-function randomElem(theArray) {
-    var randomIndex = Math.floor(Math.random() * theArray.length);
-    return theArray[randomIndex];
-}
-var positions = [103, 458, 472, 458];
-var randomPosition = randomElem(positions);
