e7b47d52292974038c87d940565476e8463107e8
gerardo778 <54380051+gerardo778@users.noreply.github.com>
2019-08-28 21:42:52 -0600

    Delete generico.ts

--- generico.ts
+++
@@ -1,8 +0,0 @@
-//Ejemplo de generico
-function randomElem<T>(theArray: T[]): T {
-    let randomIndex = Math.floor(Math.random() * theArray.length);
-    return theArray[randomIndex];
-}
-
-let positions: number[] = [103, 458, 472, 458];
-let randomPosition: number = randomElem(positions); 
