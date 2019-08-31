e7f82fc0cfc82374e84cdba080b3ff63240284b6
gerardo778 <54380051+gerardo778@users.noreply.github.com>
2019-08-31 17:01:49 -0600

    for of

---
+++ Libro 2/forof.ts
@@ -0,0 +1,11 @@
+//For of por valor de variable dentro de un array
+let	list	=	["a",	"b",	"c"];
+for	(let	b	of	list)	{
+				console.log(b);	//	a,	b,	c
+}
+
+//For of por valor de una variable de string
+let	string	=	"is	it	me	you're	looking	for?";
+for	(let	char	of	string)	{
+				console.log(char);	
+}
