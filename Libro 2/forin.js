b5ae12f1d247516f53ea3856b650b948cac8ce16
gerardo778 <54380051+gerardo778@users.noreply.github.com>
2019-08-31 16:57:28 -0600

    for in

---
+++ Libro 2/forin.js
@@ -0,0 +1,10 @@
+//For in por valor de variable
+var list = { a: 1, b: 2, c: 3 };
+for (var i in list) {
+    console.log(list[i]); //  1,	2,	3
+}
+//For in por indice
+var list2 = { d: 1, e: 2, f: 3 };
+for (var z in list2) {
+    console.log(z); //    d,	e,	f
+}
---
+++ Libro 2/forin.ts
@@ -0,0 +1,11 @@
+//For in por valor de variable
+let	list	=	{a:	1,	b:	2,	c:3};
+for(let	i	in	list){
+		console.log(list[i]);	//  1,	2,	3
+}
+
+//For in por indice
+let	list2	=	{d:	1,	e:	2,	f:3};
+for(let	z	in	list2){
+		console.log(z);	  //    d,	e,	f
+}
