ab10e680985773d9dae2ff8aa40193995f32b08f
gerardo778 <54380051+gerardo778@users.noreply.github.com>
2019-09-02 21:19:22 -0600

    decoradores de propiedades

---
+++ Libro 2/propdecor.ts
@@ -0,0 +1,10 @@
+function PropertyDecorator(
+    target: Object, 
+    propertyKey: string | symbol 
+    ) {
+    console.log("PropertyDecorator called on: ", target, propertyKey);
+}
+class PropertyDecoratorExample {
+    @PropertyDecorator
+    name!: string;
+}
