48e8e8e66531ac50472692eba6851f5f5fb007ae
gerardo778 <54380051+gerardo778@users.noreply.github.com>
2019-09-02 21:23:28 -0600

    decoradores de metodos

---
+++ Libro 2/methdecor.ts
@@ -0,0 +1,33 @@
+function MethodDecorator(
+    target: Object, // The prototype of the class
+    propertyKey: string, // The name of the method
+    descriptor: TypedPropertyDescriptor<any>
+    ) {
+    console.log("MethodDecorator called on: ", target, propertyKey, descriptor);
+}
+class MethodDecoratorExample {
+    @MethodDecorator
+    method() {
+    }
+}
+MethodDecorator called on:  { method: [Function] } method { value: [Function],
+  writable: true,
+  enumerable: true,
+  configurable: true 
+}
+
+//Estatico
+function	StaticMethodDecorator(
+    target:	Function,	//	the	function	itself	and	not	the	prototype
+    propertyKey:	string	|	symbol,	//	The	name	of	the	static	method
+    descriptor:	TypedPropertyDescriptor<any>
+    )	{
+    console.log("StaticMethodDecorator	called	on:	",	target,	propertyKey,	descriptor);
+}
+class	StaticMethodDecoratorExample	{
+    @StaticMethodDecorator
+    static	staticMethod()	{
+    }
+}
+StaticMethodDecorator	called	on:		function	StaticMethodDecoratorExample()	{
+}
